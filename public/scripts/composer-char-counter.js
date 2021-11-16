7f5dc48595fa360db896c64d32098de8bc57e637
spatel93 <sid.auburn@gmail.com>
2021-11-16 14:26:54 +0000

    implement counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  // const count = $(".counter").html();
+  // let = Number(count);
+  // console.log(count);
+
+  $("#tweet-text").on("keyup", function(){
+    let output = $(this).next().children("output");
+    let currentCount = this.value.length;
+    let total = 140 - currentCount
+    if(total < 0){
+      console.log("minus value")
+      output.val(total)
+      output.css("color","red")
+    }
+    else {
+      output.css("color","black")
+      output.val(total)
+    }
+  })
+});
