4b2eb98fdb0788f39def9e339868be088983eadd
spatel93 <sid.auburn@gmail.com>
2021-11-17 14:35:14 +0000

    implement fetching data async

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,76 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
-const createTweetElement = (tweet) => {
-  var name = tweet["user"]["name"]
-  var avatar = tweet["user"]["avatars"]
-  var handle = tweet["user"]["handle"]
-  var content = tweet["content"]["text"]
-  var date = tweet["created_at"]
+$(document).ready(function() {
   
-  console.log(name, avatar, handle, content, date)
-
-  var tweetHTML = $(`<article>
-  <header>
-    <div>          
-      <img src=${avatar}>
-      <span>${name}</span>
-    </div>
-    <span>${handle}</span>
-  </header>
-  <div>
-    <p>${content}</p>
-  </div>
-  <footer>
-    <span> ${timeago.format(date)}</span>
+  const createTweetElement = (tweet) => {
+    var name = tweet["user"]["name"]
+    var avatar = tweet["user"]["avatars"]
+    var handle = tweet["user"]["handle"]
+    var content = tweet["content"]["text"]
+    var date = tweet["created_at"]
+    
+    var tweetHTML = $(`<article>
+    <header>
+      <div>          
+        <img src=${avatar}>
+        <span>${name}</span>
+      </div>
+      <span>${handle}</span>
+    </header>
     <div>
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-repeat"></i>
-      <i class="fa-solid fa-heart"></i>
+      <p>${content}</p>
     </div>
-  </footer>
-</article>`)
-
-  $('.tweets-section').append(tweetHTML)
-  return tweetHTML
-
-}
-
-const renderTweets = (arrOfTweets) => {
-  for(var obj of arrOfTweets){
-     var newTweet = createTweetElement(obj);
-     $(".tweets-section").append(newTweet);
+    <footer>
+      <span> ${timeago.format(date)}</span>
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-repeat"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>`)
+  
+    $('.tweets-section').append(tweetHTML)
+    return tweetHTML
+  
   }
-}
+  
+  const renderTweets = (arrOfTweets) => {
+    for(var obj of arrOfTweets){
+       var newTweet = createTweetElement(obj);
+       $(".tweets-section").append(newTweet);
+    }
+  }
+  
+  $(".new-tweet form").submit(function(event){
+    event.preventDefault()
+  
+    $.ajax({
+      type: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize()
+    })
+  
+  })
 
-renderTweets(data)
+  const loadtweets = () => {
+    $.ajax({
+      type: "GET",
+      url: "/tweets"
+    })
+    .then(function(data){
+      renderTweets(data);
+    })
+  }
 
+  loadtweets();
 
-
-
+})
