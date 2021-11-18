f47b5168f4ecf87ac85f0f2e8d78a99e11cd580b
spatel93 <sid.auburn@gmail.com>
2021-11-18 19:43:18 +0000

    remove dead code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-  // const count = $(".counter").html();
-  // let = Number(count);
-  // console.log(count);
-
   $("#tweet-text").on("keyup", function() {
     let output = $(this).next().children("output");
     let currentCount = this.value.length;
